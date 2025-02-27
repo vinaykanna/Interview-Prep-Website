c47e1f4b5a6f73cf7f0c37fb17e9076d6249470b
vinaykumar <vinaykanna367@gmail.com>
2025-02-27 15:53:09 +0530

    fix build errors

--- Frontend/src/components/SideNav.tsx
+++ Frontend/src/components/SideNav.tsx
@@ -1,18 +1,7 @@
-import { getTopics } from "@/utils/services";
-import { useQuery } from "@tanstack/react-query";
-
 function SideBar() {
-  const { data } = useQuery({ queryKey: ["topics"], queryFn: getTopics });
-
   return (
     <aside className="bg-gray-200 w-3xs h-full fixed left-0 top-0">
-      {data?.data?.map((topic: any) => {
-        return (
-          <div key={topic.id} className="p-2">
-            {topic.name}
-          </div>
-        );
-      })}
+      <h1>Sidebar</h1>
     </aside>
   );
 }
