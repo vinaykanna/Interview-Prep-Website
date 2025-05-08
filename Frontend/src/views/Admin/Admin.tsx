05b4ebbd4dde574cc7e66e843e2f8745fc4df7fd
vinaykumar <vinaykanna367@gmail.com>
2025-05-08 13:25:45 +0530

    add link to interview admin title to go to home

--- Frontend/src/views/Admin/Admin.tsx
+++ Frontend/src/views/Admin/Admin.tsx
@@ -1,5 +1,5 @@
 import FloatingActionsMenu from "@/components/FloatingMenu";
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 import UploadAsset from "../UploadAsset";
 import { useState } from "react";
 import { useInterviewPrepAdminContext } from "@/InterviewPrepAdminContext";
@@ -19,7 +19,9 @@ function Admin() {
 
   return (
     <section className="max-w-5xl mx-auto p-5">
-      <h1 className="text-4xl text-center font-bold">Interview Prep Admin</h1>
+      <Link to="/admin">
+        <h1 className="text-3xl text-center font-bold">Interview Prep Admin</h1>
+      </Link>
       <Outlet />
       <FloatingActionsMenu setIsUploadAssetOpen={setIsUploadAssetOpen} />
       {isUploadAssetOpen && (
