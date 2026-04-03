61cb301737f7a88d1de5b4f1f6c5dedc47eb6377
vinaykumar <vinaykanna367@gmail.com>
2026-04-04 01:56:58 +0530

    update http url

--- Frontend/src/utils/http.ts
+++ Frontend/src/utils/http.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_API_URL = import.meta.env.DEV
   ? "http://localhost:4000"
-  : "https://interview-prep-website-a1ztabpq51nz.vinaykanna.deno.net";
+  : "https://interview-prep-website-be.vinaykanna.deno.net";
 
 const http = axios.create({
   baseURL: BASE_API_URL,
